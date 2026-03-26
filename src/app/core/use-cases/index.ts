8eb90ade4762e99df2afd5299166f8814825cf03
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-03-25 18:28:03 -0600

    Caso de uso con la versión deprecated de Assistant API

--- src/app/core/use-cases/index.ts
+++ src/app/core/use-cases/index.ts
@@ -6,3 +6,5 @@ export * from './audios/text-to-audio.use-case';
 export * from './audios/audio-to-text.use-case';
 export * from './images/image-generation.use-case';
 export * from './images/image-variation.use-case';
+export * from './assistant/create-thread.use-case';
+export * from './assistant/post-question.use-case';
--- src/app/presentation/pages/assistantPage/assistantPage.ts
+++ src/app/presentation/pages/assistantPage/assistantPage.ts
@@ -1,9 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { ChatMessage, MyMessage, TypingLoader, TextMessageBox } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-assistant-page',
-  imports: [],
+  imports: [ChatMessage, MyMessage, TypingLoader, TextMessageBox],
   templateUrl: './assistantPage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AssistantPage {}
+export default class AssistantPage implements OnInit {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+  public threadId = signal<string | undefined>(undefined);
+
+  ngOnInit(): void {
+    this.openAiService.createThread().subscribe((id) => {
+      this.threadId.set(id);
+    });
+  }
+
+  handleMessage(question: string) {
+    this.isLoading.set(true);
+    this.messages.update((prev) => [...prev, { text: question, isGpt: false }]);
+    this.openAiService.postQuestion(this.threadId()!, question).subscribe((replies) => {
+      this.isLoading.set(false);
+
+      for (const reply of replies) {
+        for (const message of reply.content) {
+          this.messages.update((prev) => [
+            ...prev,
+            { text: message, isGpt: reply.role === 'assistant' },
+          ]);
+        }
+      }
+    });
+  }
+}
